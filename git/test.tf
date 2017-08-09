9fa6d602406a835e0de65fe4f243e61562d21cf1
harish <harishkumarswamy@gmail.com>
2017-08-09 15:55:08 +0530

    git code

---
+++ git/test.tf
@@ -0,0 +1,10 @@
+#provider "github" {
+#  token        = "af0639a41ec997790c164118e887e454ae20e796"
+#  organization = "harishsrs"
+#}
+#resource "github_repository" "example" {
+#  name        = "example"
+#  description = "My awesome codebase"
+#  has_wiki    = "true"
+#}
+
